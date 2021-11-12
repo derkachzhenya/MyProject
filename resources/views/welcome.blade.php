6674a80d26368a4d1ae9911ba7c08ad296f284a1
Yevhen <zheka4573@gmail.com>
2021-11-12 11:06:29 +0200

    v0.1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compate"ibl content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+      <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  
+  <meta name="yandex-verification" content="b71ac566e90cd3ad" />
+  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122639346-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-122639346-1');
+</script>
+  
+  
+  
+  <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(85759615, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/85759615" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+    <title>Kostenlos in Deutschland, kleidung, elektronik, autos  | Develn</title>
+  <meta name="description" content="Dies ist ein neues einzigartiges Portal für die Veröffentlichung von Anzeigen, auf dem Sie ganz einfach Produkte kaufen oder verkaufen können. Jetzt ist es ganz einfach, eine Anzeige zu platzieren. Es dauert nur ein paar Minuten. Viel Spaß beim Einkaufen und erfolgreiches Verkaufen in Deutschland!">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #4747a9">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="#"><h1>Develn.de</h1></a>
+      <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+        <span class="navbar-toggler-icon "></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav col-md-12">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#">Verkaufen</a>
+          </li>
+          <li class="nav-item col-8 ">
+            <a class="nav-link text-white" href="#">Anmelden</a>
+          </li>
+          <li class="nav-item col-2">
+                <a class="nav-link text-white " href="#">Mein Develn</a>
+          </li>
+          <li class="nav-item  ">
+            <a class="nav-link text-white " href="#"><svg xmlns="http://www.w3.org/2000/svg" font-weight: bold; width="22" height="22"  fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg></a>
+            
+      </li>
+          </ul>
+      </div>  
+    </div>
+  </nav>
+
+
+<div class="container col-10 col-sm-8 col-md-6 mt-4">
+  <form class="d-flex">
+    <input class="form-control me-2" type="search" placeholder="Was suchen Sie?" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit">Finden</button>
+  </form>
+</div>
+
+
+<div class="container">
+ 
+<div class="d-flex flex-wrap container mt-3 mb-5">
+
+<?php
+$arr = ['', 'Computer & Büro', 'Kleidung & Accessoires','Smartphones & Handys', 'Haus & Garten', 
+'Auto & Motorrad','Elektroartikel', 'Baby & Kind', 'Bücher & Zeitschriften', 'Tiere', 'Alle Kategorien'];
+for($i=1; $i<=10; $i++):
+    ?> 
+<div class="card col-5 col-md-2 col-sm-5" mb-10 >
+<img src="img/<?php echo $i ?>.jpg" class="rounded mx-auto d-block" style="width: 5rem;" alt="...">
+<div class="title text-center"><a href=""><?php echo $arr[$i]?></a></div>
+</div>
+
+<?php 
+endfor;
+?>
+
+</div>
+</div>
+
+<hr>
+<footer class="container mt-5">
+  <p class="float-end"><a href="#">Back to top</a></p>
+  <p>Copyright © 2021 Develn.de, Inc. · <a href="header">Develn - AGB</a> · <a href="#">Datenschutzerklärung</a></p>
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
